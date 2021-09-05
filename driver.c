3614d50e368a9adf0e457753cafb039e501a20c7
Duong Hoang <hoang@hoare7.cs.umsl.edu>
2021-09-05 14:33:23 -0500

    Implement help menu and function to verify argument

--- driver.c
+++ driver.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 #include <unistd.h> 
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+void helpmenu(char* driver){
+	printf("This program is used to test the functionality\n");
+	printf("of logging library. This program will read a set\n");
+	printf("of messages from a file call 'message.txt' and output\n");
+	printf("the respective message in another form.\n");
+	printf("%s [-h] [-t sec] [logfile]\n",driver);
+	printf("\t [-h]: print help menu.\n");
+	printf("\t [-t sec]: specify the time sleep between each logging\n");
+	printf("\t [logfile]: specify the output file\n");
+
+}
+
+int validSec(char* sec){
+	int size = strlen(sec);
+	int i = 0;
+	while(i < size){
+		if(!isdigit(sec[i]))
+			return 0;
+		i++;		
+	}
+	return 1;
+}
 
 int main(int argc, char** argv){
 	int option;
 	int sec;
 	while((option = getopt(argc, argv, "ht:")) !=-1){
 		switch(option){
-			case 'h': printf("help menu");
-				break;
+			case 'h':
+				helpmenu(argv[0]);
+				return 1; 
 			case 't':
-				sec = atoi(optarg);
-				printf("Receive parameter sec = %d",sec);
+				if(validSec(optarg)){
+					sec = atoi(optarg);
+					printf("Received sec valued: %d\n",sec);
+				}else{
+					fprintf(stderr,"Error: %s is not a valid number\n",optarg);
+					return EXIT_FAILURE;
+				}
 				break;
 			case '?':
 				if(optopt == 't')
@@ -20,7 +50,8 @@ int main(int argc, char** argv){
 				else
 					fprintf(stderr, "Unrecognized option: -%c\n", optopt);
 				return EXIT_FAILURE;
-
+			default: 
+				printf("From default");
 		}
 	}
 	printf("\nDone while loop");
