cd6da6b33e42de443438ccb335f6115c1d019c8a
Duong Hoang <hoang@hoare7.cs.umsl.edu>
2021-09-07 01:49:01 -0500

    Add addmsg() and getlog() function, implement test in driver.c

--- driver.c
+++ driver.c
@@ -3,12 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "log.h"
+#include <errno.h>
+
+extern int errno;
+char* message[] = {"I This program has bug",
+			"W Bug on line 3",
+			"E Cannot find an error",
+			"W Another bug on line 6",
+			"I Need to update C version",
+			"F The program has to be stopped"};
+#define n_array (sizeof(message)/sizeof(char*))
 
 void helpmenu(char* driver){
 	printf("This program is used to test the functionality\n");
-	printf("of logging library. This program will read a set\n");
-	printf("of messages from a file call 'message.txt' and output\n");
-	printf("the respective message in another form.\n");
+	printf("of logging library. This program will generate a set\n");
+	printf("of messages and output the respective message in another\n");
+	printf("form.\n");
 	printf("%s [-h] [-t sec] [logfile]\n",driver);
 	printf("\t [-h]: print help menu.\n");
 	printf("\t [-t sec]: specify the time sleep between each logging\n");
@@ -30,7 +41,12 @@ int validSec(char* sec){
 int main(int argc, char** argv){
 	int option;
 	int sec;
+	int i = 0;
+	char line[60] = {"\0"};
+	char type;
+	char msg[58] = {"\0"}; 
 	char* logfile;
+	char* log;
 	while(optind < argc){
 		if((option = getopt(argc, argv, "ht:")) !=-1){
 			switch(option){
@@ -40,7 +56,7 @@ int main(int argc, char** argv){
 				case 't':
 					if(validSec(optarg)){
 						sec = atoi(optarg);
-						printf("Received sec valued: %d\n",sec);
+						printf("Received sec value: %d\n",sec);
 					}else{
 						fprintf(stderr,"Error: %s is not a valid number\n",optarg);
 						return EXIT_FAILURE;
@@ -66,6 +82,26 @@ int main(int argc, char** argv){
 		}
 		
 	}
-	printf("\nDone while loop");
+	while(i<n_array){
+		memset(line, 0, strlen(line));
+		memset(msg, 0, strlen(msg));
+		strcpy(line, message[i]);	
+		type = line[0];
+		strcpy(msg, &line[2]);
+		if(addmsg(type,msg) == -1){
+			fprintf(stderr,"%s: ",argv[0]);
+			perror("Error");
+			return EXIT_FAILURE;
+		}
+		i++;
+	}	
+	log = getlog();
+	if(log == NULL){
+		fprintf(stderr,"%s: ",argv[0]);
+                perror("Error");
+                return EXIT_FAILURE;
+	}else
+		printf("%s", log);		
+
 	return EXIT_SUCCESS;
 }
--- loglib.c
+++ loglib.c
@@ -1,6 +1,9 @@
 #include <stdlib.h> 
 #include <string.h>
 #include "log.h"
+#include <time.h>
+#include <errno.h>
+#include <stdio.h> 
 
 typedef struct list_struct{
 	data_t item;
@@ -9,8 +12,29 @@ typedef struct list_struct{
 
 static log_t *headptr = NULL;
 static log_t *tailptr = NULL;
+static int logsize = 0; 
 
 int addmsg(const char type, const char* msg){
+	time_t t = time(NULL);	
+	log_t *newnode;
+	int nodesize;
+	
+	nodesize = sizeof(log_t) + strlen(msg) + 2;
+	if((newnode = (log_t *)(malloc(nodesize))) == NULL)
+		return -1; //couldn't add node, malloc will automatically set errno
+	
+	newnode->item.time = t;
+	newnode->item.type = type;
+	newnode->item.string = (char *)newnode + sizeof(log_t);
+	strcpy(newnode->item.string, msg);
+	newnode->next = NULL;
+
+	if(headptr == NULL)
+		headptr = newnode;
+	else
+		tailptr->next = newnode;
+	tailptr = newnode;
+	logsize = logsize + strlen(msg) + 41;	
 	return 0; //if successfull 
 
 }
@@ -19,7 +43,44 @@ void clearlog(void){
 }
 
 char *getlog(void){
-	return NULL;
+	char* result;
+	time_t t;
+	struct tm* local;
+	log_t *temptr = headptr;
+	if((result = (char* )malloc(logsize)) == NULL)
+		return NULL;
+	//temptr = NULL;//test errno
+	if(temptr == NULL){
+		errno = EADDRNOTAVAIL;
+		return NULL;
+	}
+	while(temptr != NULL){
+		char time[30] = {"\0"};
+		char *msg;
+		char type[10] = {"\0"};
+		char temp = temptr->item.type;
+		if((msg = (char* )malloc((strlen(temptr->item.string)+1)*sizeof(char))) == NULL)
+			return NULL;
+		t = temptr->item.time;
+		local = localtime(&t);
+		strcpy(time,asctime(local));
+		time[strlen(time)-1] = '\0';
+		strcpy(msg,temptr->item.string);
+		if(temp == 'I')
+			strcpy(type, ": INFO: ");
+		else if(temp == 'W')
+			strcpy(type, ": WARN: ");	
+		else if(temp == 'E')
+			strcpy(type, ": ERROR: ");
+		else
+			strcpy(type, ": FATAL: ");
+		strcat(result,time);
+		strcat(result,type);
+		strcat(result,msg);
+		strcat(result,"\n");
+		temptr = temptr->next;				
+	}
+	return result;
 }
 
 int savelog(char *filename){
